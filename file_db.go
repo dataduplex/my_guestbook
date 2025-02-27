54a31c4eb0a18f3125bbae41337443378cb1ec21
Jayanthan Elumalai <jayanth2@gmail.com>
2025-02-26 22:10:30 -0800

    review comments

--- file_db.go
+++ file_db.go
@@ -8,23 +8,35 @@ import (
 	"strconv"
 )
 
+// Needs a method to read and load guests from file DB.
+// Currently, any restart of the application will erase the data saved to DB.
 type FileDB struct {
 	dir string
 }
 
 func NewFileDB(dir string) (*FileDB, error) {
+	// perhaps validate if path exists, if not, create it here
 	return &FileDB{
 		dir: dir,
 	}, nil
 }
 
 func (f *FileDB) SaveGuests(ctx context.Context, g *Guests) error {
+	// There are no functions to load existing "db" file to map
+	// The below line will erase the contents of any existing "db" file - we lose data.
+	// If for any reason, the application crashes while writing to DB - we lose data.
+	// Perhaps a safer option is to create a new file each time with current timestamp
+	// After finish writing, save it with an extension (For example - "db.<timestamp>.FULL" to indicate it has all data saved)
+	// when reading from DB, use the file with latest timestamp
+	// old files can be purged in a separate thread or a separate cron job.
 	fh, err := os.Create(path.Join(f.dir, "db"))
 	if err != nil {
+		// return error to caller instead of panic
 		panic(err)
 	}
 	defer fh.Close()
 
+	// as mentioned in another comment, possible deadlock scenario with locking twice here at (1) and (2)
 	for name := range g.Guests { // (1)
 		fh.WriteString(name)
 		fh.WriteString(" ")
--- server.go
+++ server.go
@@ -38,24 +38,33 @@ func (s *Server) Run(ctx context.Context) {
 		case <-ticker.C:
 			slog.InfoContext(ctx, "persisting guests to db")
 			err := s.db.SaveGuests(ctx, s.guests)
+			// Check for error here
+			// Print success only when error is nil, else print the error message
 			slog.InfoContext(ctx, "PERSISTED GUESTS TO DB", "error", err.Error())
 		}
 	}
 }
 
 func (s *Server) handlerGetRoot(rw http.ResponseWriter, req *http.Request) {
+	// 1. unsafe operation as multiple threads can simultaneously increment s.visits
+	// 2. this shall be moved to a function of it's own (example func (*Server) IncrementVisits() int) which is protected by a write lock
+	// 3. save the visits to a local variable after increment since it's used to return the response
 	s.visits++
 
 	var buf bytes.Buffer
 
 	buf.WriteString("Visits: ")
+	// this s.visits may not be accurate as the increment step earlier and the read step below are not an atomic operation
+	// after increment, save it a local var and use it here.
 	buf.WriteString(strconv.Itoa(s.visits))
 	buf.WriteString("\n\n")
 
 	buf.WriteString("Guests:\n")
 
-	for name := range s.guests.Guests {
-		if s.guests.IsSpecial(name) {
+	// possible deadlock here as Guests and IsSpecial are read locking in succession without releasing
+	// deadlock will happen when another thread tries to acquire a write lock between (1) and (2)
+	for name := range s.guests.Guests { // (1) locking here
+		if s.guests.IsSpecial(name) { // (2) locking again here without releasing the earlier one
 			buf.WriteString("* ")
 		} else {
 			buf.WriteString("- ")
@@ -68,7 +77,10 @@ func (s *Server) handlerGetRoot(rw http.ResponseWriter, req *http.Request) {
 }
 
 func (s *Server) handlerPostSign(rw http.ResponseWriter, req *http.Request) {
+	// validate "name" to make sure it is not empty
 	name := req.FormValue("name")
+
+	// Handle error here, return without updating the map if an invalid input was passed.
 	special, _ := strconv.ParseBool(req.FormValue("special"))
 
 	s.guests.Add(name, special)
